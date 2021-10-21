8b61d2c58359b722d44e759223787f33d11f1831
StephHoel <steph.hoel@gmail.com>
2021-10-20 21:30:00 -0300

    Stage 1

---
+++ src/controllers/ProfileUserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { ProfileUserService } from "../services/ProfileUserService";
+
+class ProfileUserController {
+   async hundle(request: Request, response: Response) {
+      const { user_Id } = request;
+
+      const service = new ProfileUserService();
+
+      const result = await service.execute(user_Id);
+
+      return response.json(result);
+
+   }
+}
+
+export { ProfileUserController };
---
+++ src/prisma/index.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@prisma/client/index.js";
+
+const prismaClient = new PrismaClient();
+
+export default prismaClient;
